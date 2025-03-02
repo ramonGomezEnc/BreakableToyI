62eb0a89e9f538700284eaf9dc132791923bf9ae
ramonGomezEnc <ramon.gomez@encora.com>
2025-03-02 13:13:16 -0700

    fix: add comments

--- backend/src/main/java/com/todo/backend/task/TaskController.java
+++ backend/src/main/java/com/todo/backend/task/TaskController.java
@@ -1,5 +1,7 @@
 package com.todo.backend.task;
 
+import com.todo.backend.task.dto.TaskDTO;
+import com.todo.backend.task.dto.TaskResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/todos")
+@RequestMapping("/api/v2/todos")
 public class TaskController {
     private final TaskService service;
 
@@ -17,8 +19,9 @@ public class TaskController {
         this.service = service;
     }
 
+    // Retrieves all tasks with optional filtering, sorting, and pagination
     @GetMapping
-    public ResponseEntity<TaskResponse> getAllTasks(
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String priority,
             @RequestParam(required = false) Boolean isCompleted,
@@ -28,45 +31,41 @@ public class TaskController {
         return new ResponseEntity<>(service.getTasks(name, priority, isCompleted, sortBy, order, page), HttpStatus.OK);
     }
 
+    // Creates a new task
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task payload) {
+    public ResponseEntity<TaskResponseDTO> createTask(@RequestBody TaskDTO payload) {
         return new ResponseEntity<>(service.createTask(payload), HttpStatus.CREATED);
     }
 
-    // Only for testing
-    @PostMapping("/list")
-    public ResponseEntity<List<Task>> createTasks(@RequestBody List<Task> tasks) {
-        List<Task> createdTasks = service.createTasks(tasks);
-        return new ResponseEntity<>(createdTasks, HttpStatus.CREATED);
-    }
-
-
+    // Updates task content
     @PutMapping("/{taskId}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody Task payload) {
+    public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable Long taskId, @RequestBody TaskDTO payload) {
         return new ResponseEntity<>(service.updateTaskContent(taskId, payload), HttpStatus.OK);
     }
 
+    // Updates task completion status
     @PatchMapping("/{taskId}/{status}")
-    public ResponseEntity<Task> updateTaskStatus(@PathVariable Long taskId, @PathVariable String status) {
+    public ResponseEntity<TaskResponseDTO> updateTaskStatus(@PathVariable Long taskId, @PathVariable String status) {
         return new ResponseEntity<>(service.updateTaskStatus(taskId, status), HttpStatus.OK);
     }
 
+    // Deletes a task
     @DeleteMapping("/{taskId}")
     public ResponseEntity<String> deleteTask(@PathVariable Long taskId) {
         return new ResponseEntity<>(service.deleteTask(taskId), HttpStatus.OK);
     }
 
+    // Calculates and returns the average time taken to complete tasks
     @GetMapping("/averageTime")
     public ResponseEntity<String> getAverageTime() {
         String averageTime = service.calculateAverageTime();
         return new ResponseEntity<>(averageTime, HttpStatus.OK);
     }
 
+    // Calculates and returns the average completion time for tasks with a specific priority (High, Medium, Low)
     @GetMapping("/averageTime/{priority}")
     public ResponseEntity<String> getAverageTimeByPriority(@PathVariable String priority) {
         String averageTime = service.calculateAverageTimeByPriority(priority);
         return new ResponseEntity<>(averageTime, HttpStatus.OK);
     }
-
 }
-
